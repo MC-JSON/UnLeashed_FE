1a31d1b15bf3b86e302f7a7d946308307508759d
Jason McCoy <jwmccoy29@gmail.com>
2022-05-06 09:35:12 -0500

    file structure update and nav build

--- unleashed/src/App.js
+++ unleashed/src/App.js
@@ -1,5 +1,10 @@
 import './styles/App.css'
 import { Route, Routes } from 'react-router-dom'
+import Nav from './components/Nav'
+import Home from './pages/Home'
+import Airline from './pages/Airlines'
+import City from './pages/City'
+import Country from './pages/Country'
 
 function App() {
   return (
@@ -7,11 +12,11 @@ function App() {
       <Nav />
       <main>
         <Routes>
-          {/* <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/airlines" element={<Airline />} />
           <Route path="/cities" element={<City />} />
           <Route path="/countries" element={<Country />} />
-          <Route path="*" element={<Home />} /> */}
+          <Route path="*" element={<Home />} />
         </Routes>
       </main>
     </div>
