ae952e9e744818b79a955aa65b183b7de09b9292
Jason McCoy <jwmccoy29@gmail.com>
2022-05-09 15:12:11 -0500

    layout render rework

--- unleashed/src/components/AirLayout.jsx
+++ unleashed/src/components/AirLayout.jsx
@@ -2,12 +2,11 @@ const AirLayout = (props) => {
   return (
     <div className="card">
       <div className="img-wrapper">
-        <img src={props.logo} alt="Logo" />
+      <a href={props.url}><img src={props.logo} alt="Logo" /></a>
       </div>
       <div className="info-wrapper">
         <h2>{props.name}</h2>
         <p>{props.reservations}</p>
-        <a href={props.url}>{props.url}</a>
       </div>
       <br />
     </div>
--- unleashed/src/components/CountryLayout.jsx
+++ unleashed/src/components/CountryLayout.jsx
@@ -2,12 +2,11 @@ const CountryLayout = (props) => {
   return (
     <div className="card">
       <div className="img-wrapper">
-        <img src={props.flag_img} alt="Flag" />
+      <a href={props.url}><img src={props.flag_img} alt="Flag" /></a>
       </div>
       <div className="info-wrapper">
         <h2>{props.name}</h2>
         <p>{props.requirements}</p>
-        <a href={props.url}>{props.url}</a>
       </div>
       <br />
     </div>
